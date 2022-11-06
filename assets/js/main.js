9e0f3f4a701902dcd54cabe9d4402243d65507a1
osimi-os <hung2262002@gmail.com>
2022-11-06 11:27:02 +0700

    done nội dung chương trình tập

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ const scrollActive = () =>{
 }
 window.addEventListener('scroll', scrollActive)
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP or button call :)) ===============*/ 
 const scrollUp = () =>{
 	const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
